2e0d7593b748b3a2432087a31e873d05d293bfb6
Ruben Fernandez <rubenfernandezp03@gmail.com>
2023-09-18 02:35:08 +0200

    version 1.1 con tests

--- src/test/java/org/rubenfernandez/ColaPrioridadTest.java
+++ src/test/java/org/rubenfernandez/ColaPrioridadTest.java
@@ -2,12 +2,24 @@ package org.rubenfernandez;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColaPrioridadTest {
 
     @Test
     void anadirProceso() {
+        ArrayList<Proceso> cola =new ArrayList<>();
+        UUID uuid=UUID.randomUUID();
+        Proceso p=new Proceso(uuid,"p1",100,7);
+        cola.add(p);
+        assertTrue(cola.contains(p));
+        assertEquals(1,cola.size());
+
+
+
 
 
         
@@ -15,15 +27,47 @@ class ColaPrioridadTest {
     }
 
     @Test
+
     void ordenarCola() {
-    }
+            ArrayList<Proceso> cola = new ArrayList<>();
+            UUID uuid1 = UUID.randomUUID();
+            UUID uuid2 = UUID.randomUUID();
+            Proceso p1 = new Proceso(uuid1, "p1", 100, 7);
+            Proceso p2 = new Proceso(uuid2, "p2", 100, 8);
+            assertTrue( p1.prioridad<= p2.prioridad);
+        }
+
 
     @Test
     void elimiarProceso() {
+
+        ArrayList <Proceso> cola = new ArrayList<>();
+        UUID uuid1=UUID.randomUUID();
+        UUID uuid2=UUID.randomUUID();
+        UUID uuid3=UUID.randomUUID();
+        UUID uuid4=UUID.randomUUID();
+        cola.add(new Proceso (uuid1,"P1",100,8) );
+        cola.add(new Proceso(uuid2,"P2",70,7) );
+        cola.add(new Proceso(uuid3,"P3",56,7) );
+        cola.add(new Proceso(uuid4,"P4",54,7) );
+        cola.remove(0);
+        assertFalse(cola.contains(uuid1));
+        assertEquals(3,cola.size());
+
     }
 
     @Test
     void ejecutarProceso() {
+        ArrayList<Proceso> cola=new ArrayList<>();
+        UUID uuid1=UUID.randomUUID();
+        Proceso p=new Proceso(uuid1,"p1",1,7);
+        cola.add(p);
+        p.quantum--;
+        if(p.quantum==0){
+            cola.remove(p);
+        }
+        assertEquals(0,cola.size());
+
     }
 
 
